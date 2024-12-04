1b3f797e2a5132e05438d9eace47007162efe93f
LT1113 <dayan.cherrez@sofka.com.co>
2024-12-04 16:48:43 -0500

    Tittle: Subida de la pagina web
    Descripcion: Pagina web basica

---
+++ script.js
@@ -0,0 +1,5 @@
+// Cambiar mensaje al hacer clic
+document.getElementById("changeMessage").addEventListener("click", function () {
+    document.getElementById("message").textContent = "¡Has hecho clic en el botón!";
+});
+
