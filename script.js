0ecc15e5efe01616b4108018d637d3c31c12f48b
Isabel <isabel.higuera@sofka.com.co>
2024-12-06 07:29:20 -0500

    Title: Se agrega accion al boton

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 document.getElementById("changeMessage").addEventListener("click", function () {
     document.getElementById("message").textContent = "¡Has hecho clic en el botón felicidades!";
 });
+document.getElementById("changeMessage1").addEventListener("click", function () {
+    document.getElementById("message").textContent = "¡Has hecho clic en el botón felicidades!";
+});
+
 
